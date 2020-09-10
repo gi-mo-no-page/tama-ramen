3863f39eaff5b637b277c97506d5da2d3c67d683
gi-mo-no-page <gi-mo-no-page@users.noreply.github.com>
2020-09-10 15:36:37 +0900

    wentの表示、削除

--- app/controllers/wents_controller.rb
+++ app/controllers/wents_controller.rb
@@ -1,4 +1,25 @@
 class WentsController < ApplicationController
   def index 
   end
+
+  def create
+    @shop = Shop.find(params[:shop_id])
+    went = Went.new(
+      user_id: current_user.id,
+      shop_id: @shop.id
+    )
+    went.save
+    redirect_to shop_path(@shop.id)
+  end
+
+  def destroy
+    @shop = Shop.find(params[:id])
+    went = Went.find_by(
+      user_id: current_user.id,
+      shop_id: @shop.id
+    )
+    went.destroy
+    redirect_to shop_path(@shop.id)
+  end
+
 end
