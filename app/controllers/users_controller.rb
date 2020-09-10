6a2bf1d522634cc740d9d0e7445e3b1e00de704c
gi-mo-no-page <gi-mo-no-page@users.noreply.github.com>
2020-09-10 11:24:00 +0900

    自己紹介文の作成と表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
 
   def update
     if current_user.update(user_params)
-      redirect_to root_path
+      redirect_to user_path(current_user.id)
     else
       render :edit
     end
@@ -19,7 +19,7 @@ class UsersController < ApplicationController
 
   private
   def user_params
-    params.require(:user).permit(:nickname, :email, :avatar)
+    params.require(:user).permit(:nickname, :email, :avatar, :text)
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -68,6 +68,7 @@ ActiveRecord::Schema.define(version: 2020_09_10_000123) do
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
     t.string "email", default: "", null: false
+    t.string "text"
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
