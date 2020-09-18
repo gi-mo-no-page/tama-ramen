e05aaf0cd395a2776c2bca00526aed3f2e3edf1d
gi-mo-no-page <gi-mo-no-page@users.noreply.github.com>
2020-09-18 22:48:20 +0900

    userモデルok

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,13 +2,9 @@ FactoryBot.define do
   factory :user do
     nickname              { 'yamada' }
     email                 { 'aaaaaaa@gmail.com' }
-    text                  { 'こんにちは' }
     password              { '123yamada' }
     password_confirmation { password }
 
-    after(:build) do |item|
-      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
-    end
     
   end
 end
