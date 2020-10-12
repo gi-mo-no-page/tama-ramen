cc153806994dbae137fe46243a54e75350e683f6
gi-mo-no-page <gi-mo-no-page@users.noreply.github.com>
2020-10-12 12:05:55 +0900

    commit

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -61,8 +61,8 @@
 #   }
 server '54.249.195.94', user: 'ec2-user', roles: %w{app db web}
 
-set :ssh_options, {
-  keys: [ENV.fetch('PRODUCTION_SSH_KEY').to_s],
-  forward_agent: true,
-  auth_methods: %w[publickey]
-}
+# set :ssh_options, {
+#   keys: [ENV.fetch('PRODUCTION_SSH_KEY').to_s],
+#   forward_agent: true,
+#   auth_methods: %w[publickey]
+# }
